447190f440a0db54ef4d45040f0c3e98aef2523c
Jahanzaib Khan <168153593+jahanzaibakhan@users.noreply.github.com>
2026-02-23 16:49:49 +0500

    Rename server-list-new3.sh to server-list-new4.sh

--- server-list-new4.sh
+++ server-list-new4.sh
@@ -5,12 +5,7 @@ printf "%-15s %-12s %-12s\n" "---------" "--------" "---------"
 
 for SERVER in "$@"; do
 
-    OUTPUT=$(cng $SERVER <<EOF 2>/dev/null
-df -h /
-df -i /
-exit
-EOF
-)
+    OUTPUT=$(printf "df -h /\ndf -i /\nexit\n" | cng "$SERVER" 2>/dev/null)
 
     DISK=$(echo "$OUTPUT" | grep -Eo '[0-9]+%' | head -1)
     INODE=$(echo "$OUTPUT" | grep -Eo '[0-9]+%' | tail -1)
